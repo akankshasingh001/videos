a54f644886cb83c4f1ebee1f6d6b2c2a3fa951dd
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-08 15:21:51 -0400

    text box created and added some styling

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { searchText: '' };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+  };
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.searchText}
+              onChange={e => this.setState({ searchText: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
