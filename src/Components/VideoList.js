d7ff986b3abd3b32dcd793b78e397a909045ede4
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-08 18:03:22 -0400

    fetch images and title from api

--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
   const listOfVideos = videos.map(video => {
-    return <VideoItem />;
+    return <VideoItem video={video} />;
   });
   return <div>{listOfVideos}</div>;
 };
